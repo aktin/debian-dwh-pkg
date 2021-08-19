12dc715e3b111f26aefbbfb8c0d80d9d0e896f5f
Alexander Kombeiz <akombeiz@ukaachen.de>
2021-08-19 11:27:27 +0200

    added missing copying of standalone.xml.patch

--- docker/build.sh
+++ docker/build.sh
@@ -25,23 +25,28 @@ export DWHIMAGENAMESPACE="$(echo "${PACKAGE}" | awk -F '-' '{print "ghcr.io/"$1"
 # Load common linux files
 . "$(dirname "${DIR}")/common/build.sh"
 
+# Prepare wildfly docker
 mkdir -p "${DBUILD}/wildfly"
 sed -e "s|__BASEIMAGE__|${I2B2IMAGENAMESPACE}wildfly|g" "${DIR}/wildfly/Dockerfile" >"${DBUILD}/wildfly/Dockerfile"
 cp "${DRESOURCES}/aktin.properties" "${DBUILD}/wildfly/"
+cp "${DRESOURCES}/standalone.xml.patch" "${DBUILD}/wildfly/"
 download_dwh_j2ee "/wildfly"
 move_aktin_properties "/wildfly"
 move_aktin_importscripts "/wildfly"
 move_datasource_for_postinstall "/wildfly/ds"
 
+# Prepapare postgresql docker
 mkdir -p "${DBUILD}/database"
 sed -e "s|__BASEIMAGE__|${I2B2IMAGENAMESPACE}database|g" "${DIR}/database/Dockerfile" >"${DBUILD}/database/Dockerfile"
 move_database_for_postinstall "/database/sql"
 move_database_update_for_postinstall "/database/sql"
 
+# Prepare apache2 docker
 mkdir -p "${DBUILD}/httpd"
 sed -e "s|__BASEIMAGE__|${I2B2IMAGENAMESPACE}httpd|g" "${DIR}/httpd/Dockerfile" >"${DBUILD}/httpd/Dockerfile"
 config_apache2_proxy "/httpd" "wildfly"
 
+# Run docker-compose
 if [ "${FULL}" = "full" ]; then
 	cwd="$(pwd)"
 	cd "${DIR}"
